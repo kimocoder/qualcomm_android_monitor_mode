2f4e6294f4ce5d88fe3213b80fb6b88ef8ec6763
kimocoder <christian@aircrack-ng.org>
2020-03-15 02:22:43 +0100

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # qualcomm_android_monitor_mode
 Qualcomm WiFi tool adding monitor mode in Android [ Magisk ]
+
+
+
+## This is just an experiment, it may work or it may not depending on device,
+## which Android OS, drivers. Use at your own 'risk' for now. 
---
+++ config.sh
@@ -0,0 +1,104 @@
+##########################################################################################
+#
+# Qualcomm Android WiFi - Monitor mode
+# A script under testing by kimocoder, load it/control it with Magisk.
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+# To be announced. Install it with Magisk Manager.
+#
+#
+#
+#
+#
+#
+##########################################################################################
+
+##########################################################################################
+# Configs
+##########################################################################################
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=false
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "      Qcom Monitor Mode        "
+  ui_print "*******************************"
+}
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# List all directories you want to directly replace in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
+
+# This is an example
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here, it will override the example above
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
+REPLACE="
+/system/xbin
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+set_permissions() {
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Here are some examples for the set_perm functions:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+  # TODO: use proper contexts?
+  set_perm $MODPATH/system/xbin/iwpriv 0 0 0755
+  set_perm $MODPATH/system/xbin/monen 0 0 0755
+  set_perm $MODPATH/system/xbin/mondis 0 0 0755
+}
+
+##########################################################################################
+# Custom Functions
+##########################################################################################
+
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
+
